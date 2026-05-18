fa84181afbedb3a2be69415dbfd66c83bd14fe8b
jefersongomez-code <jefersongomez@emilianisomascos.edu.gt>
2026-05-18 08:08:25 -0600

    Delete p2_ej7.py

--- p2_ej7.py
+++
@@ -1,9 +0,0 @@
-# JEFERSON REYES
-# EJERCICIO5|P2
-
-#Bucle
-for i in range(1, 11):
-    print(f"\u001b[35m \n Tabla del {i}")
-
-    for j in range (1,11):
-        print(f"\u001b[34m{i} X {j} = {i*j}")
